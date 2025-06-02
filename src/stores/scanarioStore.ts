4802d2a92f865142f4850e21a73cae77683fa360
kmf <kjellmf@gmail.com>
2025-06-02 18:29:25 +0200

    Clear active item on scenario load

--- src/stores/scanarioStore.ts
+++ src/stores/scanarioStore.ts
@@ -1,12 +1,15 @@
 import { shallowRef } from "vue";
 import { MilitaryScenario } from "@orbat-mapper/msdllib";
 import { useLayerStore } from "@/stores/layerStore.ts";
+import { useSelectStore } from "@/stores/selectStore.ts";
 
 const msdl = shallowRef<MilitaryScenario>();
 
 export function useScenarioStore() {
   const layerStore = useLayerStore();
+  const selectStore = useSelectStore();
   function loadScenario(scenario: MilitaryScenario) {
+    selectStore.clearActiveItem();
     msdl.value = scenario;
     layerStore.setSideLayers(scenario);
   }
@@ -14,6 +17,7 @@ export function useScenarioStore() {
   function clearScenario() {
     msdl.value = undefined;
     layerStore.layers.clear();
+    selectStore.clearActiveItem();
   }
 
   return { loadScenario, clearScenario, msdl };
