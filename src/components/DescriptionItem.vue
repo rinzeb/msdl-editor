10f365ab9c24ad84903ccda714720aa3c0121224
KMF <kjellmf@gmail.com>
2025-06-14 11:47:05 +0200

    Add DescriptionList component and use it for ScenarioId

---
+++ src/components/DescriptionItem.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ label: string; ddClass?: string }>();
+</script>
+
+<template>
+  <div class="px-0 py-4">
+    <dt class="text-sm font-semibold">
+      <slot name="label">{{ label }}</slot>
+    </dt>
+    <dd class="mt-1 text-sm" :class="ddClass">
+      <slot />
+    </dd>
+  </div>
+</template>
--- src/components/PanelScenarioInfo.vue
+++ src/components/PanelScenarioInfo.vue
@@ -6,6 +6,8 @@ import PanelDataGrid from "@/components/PanelDataGrid.vue";
 import { Badge } from "@/components/ui/badge";
 import ShowXMLDialog from "@/components/ShowXMLDialog.vue";
 import { computed } from "vue";
+import DescriptionItem from "@/components/DescriptionItem.vue";
+import DescriptionList from "@/components/DescriptionList.vue";
 
 const { msdl } = useScenarioStore();
 const dialogStore = useDialogStore();
@@ -20,21 +22,18 @@ const myElement = computed(() => {
       <h4 class="text-sm font-bold mt-2">Scenario info</h4>
       <Badge v-if="msdl.isNETN">NETN</Badge>
     </div>
-
-    <PanelDataGrid class="mt-4 text-wrap overflow-auto">
-      <span class="font-semibold">Name</span>
-      <span>{{ msdl.scenarioId.name }}</span>
-      <span class="font-semibold">Description</span>
-      <span class="break-all">{{ msdl.scenarioId.description }}</span>
-      <span class="font-semibold">Security classification</span>
-      <span>{{ msdl.scenarioId.securityClassification }}</span>
-      <span class="font-semibold">Modification date</span>
-      <span>{{ msdl.scenarioId.modificationDate || "-" }}</span>
-      <span class="font-semibold">Type</span>
-      <span>{{ msdl.scenarioId.type || "-" }}</span>
-      <span class="font-semibold">Version</span>
-      <span>{{ msdl.scenarioId.version || "-" }}</span>
-    </PanelDataGrid>
+    <DescriptionList class="divide-y divide-border">
+      <DescriptionItem label="Name">{{ msdl.scenarioId.name || "n/a" }}</DescriptionItem>
+      <DescriptionItem label="Description">{{ msdl.scenarioId.description }}</DescriptionItem>
+      <DescriptionItem label="Security classification">
+        {{ msdl.scenarioId.securityClassification }}
+      </DescriptionItem>
+      <DescriptionItem label="Modification date">
+        {{ msdl.scenarioId.modificationDate || "n/a" }}
+      </DescriptionItem>
+      <DescriptionItem label="Type">{{ msdl.scenarioId.type || "n/a" }}</DescriptionItem>
+      <DescriptionItem label="Version">{{ msdl.scenarioId.version || "n/a" }}</DescriptionItem>
+    </DescriptionList>
 
     <div class="flex items-center mt-2">
       <Button variant="outline" class="" @click="dialogStore.toggleAssociationDialog()"
