a19783892c3d3412f00571582a30ba6e7475dd5f
rinzeb <rinzeb@users.noreply.github.com>
2025-06-20 16:45:04 +0200

    Add datafile compression (#13)
    See https://github.com/rinzeb/siso-entity-type/tree/main/packages/lib#the-siso-enums-json-file-is-quite-large-how-can-i-reduce-its-size
    
    * Update siso entity type lib to 1.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
+import { compression } from "vite-plugin-compression2";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    compression({
+      include: "**/*.json",
+      algorithms: ["gzip", "brotliCompress"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
