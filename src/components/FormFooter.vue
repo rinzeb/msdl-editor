773a9e3e704d38c7be08aa435e6edf0dc69897fe
rinzeb <rinzeb@users.noreply.github.com>
2025-06-27 15:01:52 +0200

    Add functionality to create a new, minimal MSDL scenario (#18)
    * Add menu option for creating a new MSDL scenario from scratch
    
    ---------
    
    Co-authored-by: Rinze <r@b.nl>

--- src/components/FormFooter.vue
+++ src/components/FormFooter.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { Button } from "@/components/ui/button";
 
+const props = withDefaults(defineProps<{ submitText?: string }>(), {
+  submitText: "Save",
+});
 const emit = defineEmits<{
   (e: "cancel"): void;
 }>();
@@ -8,6 +11,6 @@ const emit = defineEmits<{
 <template>
   <footer class="mt-4 flex justify-end items-center gap-1">
     <Button variant="ghost" size="sm" @click="emit('cancel')" type="button">Cancel</Button>
-    <Button size="sm" type="submit">Save</Button>
+    <Button size="sm" type="submit">{{ props.submitText }}</Button>
   </footer>
 </template>
